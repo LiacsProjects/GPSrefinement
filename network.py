af60b4cba8a5d3920951f781c3411d393cf67458
odensvanderzwan <57491728+odensvanderzwan@users.noreply.github.com>
2022-07-10 13:56:16 +0200

    Add files via upload

--- network.py
+++ network.py
@@ -1,4 +1,7 @@
 # Import required libraries
+from cProfile import label
+from turtle import color
+from joblib import PrintTime
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -10,11 +13,13 @@ import tensorflow as tf
 from tensorflow import keras
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
+from sklearn import model_selection
 from sklearn.model_selection import cross_val_score
+from sklearn.multioutput import RegressorChain
 # Import necessary modules
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import classification_report, mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
 from math import sqrt
 from sklearn.metrics import r2_score
 from datetime import timedelta
@@ -25,26 +30,27 @@ from numpy import mean
 from numpy import std
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import LinearSVR
+from sklearn.ensemble import RandomForestRegressor
 
-def gettime(ttraject,beacon1,beacon2,beacon3,beacon4,beacon5):
+def gettime(ttraject,beacon1,beacon2,beacon3,beacon4):
     Times0 = ttraject["seconds"] + ttraject["minute"]*60 + ttraject["hour"]*60*60
     Times1 = beacon1["seconds"] + beacon1["minute"]*60 + beacon1["hour"]*60*60
     Times2 = beacon2["seconds"] + beacon2["minute"]*60 + beacon2["hour"]*60*60
     Times3 = beacon3["seconds"] + beacon3["minute"]*60 + beacon3["hour"]*60*60
     Times4 = beacon4["seconds"] + beacon4["minute"]*60 + beacon4["hour"]*60*60
-    Times5 = beacon5["seconds"] + beacon5["minute"]*60 + beacon5["hour"]*60*60
-    tt = [Times0,Times1,Times2,Times3,Times4,Times5]
+    tt = [Times0,Times1,Times2,Times3,Times4]
     return tt
 
 
-def maakdata(tr,time,Groundtruth1):
+def maakdata(tr,time,Groundtruth1):  # traject tijdhuidig groundtruth
     closests = np.Infinity
     locx2 = []
     locy2 =[]
     a =  False
+    start = int(time)
     for index in tr.index:
-        time = int(time) +  int(tr.time[index])
-        Groundtruth1[(Groundtruth1['tijds'] > time)]
+        huidig = start +  int(tr.time[index])
+        Groundtruth1[(Groundtruth1['tijds'] > huidig)]
         timegroundtruth = Groundtruth1.tijds[0]   
         locatiex = Groundtruth1.locx[0]
         locatiey = Groundtruth1.locy[0]
@@ -59,7 +65,7 @@ def get_seconds(time_str):
     sss,ns = ss.split(',')
     return int(hh) * 3600 + int(mm) * 60 + int(sss)
 
-def vulbeaconcorrecties(beacon1,beacon2,beacon3,beacon4,beacon5,beacons1,timesbeacon):
+def vulbeaconcorrecties(beacon1,beacon2,beacon3,beacon4,beacons1,timesbeacon):
     corr = pd.DataFrame(columns=['time','beacon','diflat','diflon','error']) # dataframe for all the corrections per beacon 
     
     for index in beacon1.index:      # alle bestanden van de beacons afgaan om de correcties te zetten in dataframe
@@ -112,25 +118,11 @@ def vulbeaconcorrecties(beacon1,beacon2,beacon3,beacon4,beacon5,beacons1,timesbe
         time = beacon4.time[index]+ timesbeacon[4]
         new_row = {'time': time , 'beacon':4, 'diflat': verschillat,'diflon': verschillon,'error': afstand}
         corr = corr.append(new_row,ignore_index = True)
-
-    
-    
-    for index in beacon5.index  :      # een eventuele 5 want dan kan er gekeken worden naar als alle beacons op dezelfde locatie staan
-        lat = beacon5.latitude[index]
-        lon = beacon5.longitude[index] 
-        beaconhere =  (beacons1[4][0],beacons1[4][1])        # beacon 0 locatie 
-        punt = (lat,lon)                                     # measured point 
-        afstand = distance.great_circle(beaconhere,punt).m            
-        time = beacon5.time[index]+ timesbeacon[5]
-        verschillat = beacons1[4][0] - lat 
-        verschillon = beacons1[4][1] - lon 
-        new_row = {'time': time , 'beacon':5, 'diflat': verschillat,'diflon': verschillon,'error': afstand}
-        corr = corr.append(new_row,ignore_index = True)
         
     return corr
 
 def mergecorrectiontraject(trajectframe,beaconframe,alltimes):
-    frame = pd.DataFrame(columns=['beac1lat','beac1lon','beac2lat','beac2lon','beac3lat','beac3lon','beac4lat','beac4lon','beac5lat','beac5lon'])
+    frame = pd.DataFrame(columns=['beac1lat','beac1lon','beac2lat','beac2lon','beac3lat','beac3lon','beac4lat','beac4lon'])
     lats = []
     lons=[]
     temp = beaconframe
@@ -143,7 +135,7 @@ def mergecorrectiontraject(trajectframe,beaconframe,alltimes):
             beaconframe[(beaconframe['time']  >= int(huidigetijd)) ]
             lats.append(beaconframe.diflat[0])
             lons.append(beaconframe.diflon[0])
-        new_row = {'beac1lat': lats[0],'beac1lon': lons[0],'beac2lat': lats[1],'beac2lon': lons[1],'beac3lat': lats[2],'beac3lon':lons[2],'beac4lat':lats[3],'beac4lon':lons[3],'beac5lat':lats[4],'beac5lon':lons[4]}
+        new_row = {'beac1lat': lats[0],'beac1lon': lons[0],'beac2lat': lats[1],'beac2lon': lons[1],'beac3lat': lats[2],'beac3lon':lons[2],'beac4lat':lats[3],'beac4lon':lons[3]}
         frame = frame.append(new_row,ignore_index = True)
     merged = pd.concat([trajectframe,frame],axis=1,join='inner')
     return merged
@@ -179,31 +171,40 @@ def evaluate_model(X, y):
 	return results
 
 # gebruiken om hele bestanden te lezen 
-traject = pd.read_csv('001 dc81 2022 01 14 11 59 22 gps.dat',sep =",",skiprows=1) # Read trajectory
-beacon11 = pd.read_csv('011 dc81 2022 01 14 11 59 27 gps.dat',sep =",",skiprows=1) 
-beacon22 = pd.read_csv('021 dc81 2022 01 14 11 59 54 gps.dat',sep =",",skiprows=1) 
-beacon33= pd.read_csv('031 dc81 2022 01 14 12 00 27 gps.dat',sep =",",skiprows=1) 
-beacon44 = pd.read_csv('041 dc81 2022 01 14 12 00 45 gps.dat',sep =",",skiprows=1)    
-beacon55 = pd.read_csv('051 dc81 2022 01 14 12 01 17 gps.dat',sep =",",skiprows=1) 
-beaconsplekken=[(52.020093,4.268766),(52.019918,4.268743),(52.0199568,4.268911),(52.019814,4.268948),(52.0198887,4.2692081)]
+traject = pd.read_csv('051 daa0 2022 03 16 12 41 33 gps.dat',sep =",",skiprows=1) # Read trajectory
+beacon11 = pd.read_csv('010 dae4 2022 03 16 12 12 56 gps.dat',sep =",",skiprows=1) 
+beacon22 = pd.read_csv('020 db3a 2022 03 16 12 15 34 gps.dat',sep =",",skiprows=1) 
+beacon33= pd.read_csv('030 dae4 2022 03 16 12 13 23 gps.dat',sep =",",skiprows=1) 
+beacon44 = pd.read_csv('040 dac4 2022 03 16 12 13 36 gps.dat',sep =",",skiprows=1)    
+beaconsplekken=[(52.020161,4.268634),(52.020093,4.268767),(52.019981,4.268616),(52.020048,4.268484)]
 
 # volgende gebruiken voor de eerste om eerste line eruit te halen 
 columns1 = ["id","watch","year","month","day","hour","minute","seconds"]
-traject2 = pd.read_csv('001 dc81 2022 01 14 11 59 22 gps.dat',sep =" ",nrows=1,names=columns1) # Read trajectory for the first line 
-bea1 = pd.read_csv('011 dc81 2022 01 14 11 59 27 gps.dat',sep =" ",nrows=1,names=columns1)
-bea2 = pd.read_csv('021 dc81 2022 01 14 11 59 54 gps.dat',sep =" ",nrows=1,names=columns1) 
-bea3 = pd.read_csv('031 dc81 2022 01 14 12 00 27 gps.dat',sep =" ",nrows=1,names=columns1) 
-bea4 = pd.read_csv('041 dc81 2022 01 14 12 00 45 gps.dat',sep =" ",nrows=1,names=columns1)    
-bea5 = pd.read_csv('051 dc81 2022 01 14 12 01 17 gps.dat',sep =" ",nrows=1,names=columns1) 
+traject2 = pd.read_csv('051 daa0 2022 03 16 12 41 33 gps.dat',sep =" ",nrows=1,names=columns1) # Read trajectory for the first line 
+bea1 = pd.read_csv('010 dae4 2022 03 16 12 12 56 gps.dat',sep =" ",nrows=1,names=columns1)
+bea2 = pd.read_csv('020 db3a 2022 03 16 12 15 34 gps.dat',sep =" ",nrows=1,names=columns1) 
+bea3 = pd.read_csv('030 dae4 2022 03 16 12 13 23 gps.dat',sep =" ",nrows=1,names=columns1) 
+bea4 = pd.read_csv('040 dac4 2022 03 16 12 13 36 gps.dat',sep =" ",nrows=1,names=columns1)    
 
-col_list = ["timestamp","dateTime","loc(x)","loc(y)"]
+
+
+
+col_list = ["tagId","timestamp","dateTime","loc(x)","loc(y)"]
 Groundtruth = pd.read_csv("test1.csv",usecols=col_list)# ground truth opzoeken
 Groundtruth["loc(x)"] = Groundtruth["loc(x)"].div(1000) # to set the distance in meters instead of mm
 Groundtruth["loc(y)"] = Groundtruth["loc(y)"].div(1000)
+Xs = Groundtruth["loc(x)"]
+Ys = Groundtruth["loc(y)"]
+
+xys = pd.concat([Xs,Ys],axis=1,join='inner') # frame of locx and locy
+
 Groundtruth.rename(columns = {'loc(x)':'locx', 'loc(y)':'locy'}, inplace = True)
+
 
 v = Groundtruth.dateTime.str.split() 
 tijdseconden = []
+
+
 
 for index in Groundtruth.index:
     seconds = get_seconds(Groundtruth.dateTime[index])
@@ -213,49 +214,136 @@ Groundtruth['tijds'] = tijdseconden
 
 
 # preprocessen van de data en het maken van een compleet dataframe met de goede waarde voor een voorspelling 
-tijden = gettime(traject2,bea1,bea2,bea3,bea4,bea5)
+tijden = gettime(traject2,bea1,bea2,bea3,bea4)
 tijdhuidig = tijden[0]
-corrframe = vulbeaconcorrecties(beacon11,beacon22,beacon33,beacon44,beacon55,beaconsplekken,tijden)
+corrframe = vulbeaconcorrecties(beacon11,beacon22,beacon33,beacon44,beaconsplekken,tijden)
 corrframe.drop('error',inplace=True, axis=1)
-Fullframe = maakdata(traject,tijdhuidig,Groundtruth)
+
+Groundtruth.drop("timestamp", axis=1, inplace=True)
+Groundtruth.drop("dateTime", axis=1, inplace=True)
+
+Groundtruth = Groundtruth[Groundtruth["tagId"] == 26694]  # change the tagid here t
+Groundtruth.drop("tagId", axis=1, inplace=True)
+
+
+verschil = 45764 - tijden[0]
+traject = traject[traject['time']<300]
+traject = traject[traject['time'] >= int(verschil)]
+
+
+tomerge = []
+for index in traject.index:
+    huidig =  tijden[0]+  int(traject.time[index])
+    filter = Groundtruth[(Groundtruth['tijds'] > float(huidig))]
+    val = (filter['locx'].values[0],filter['locy'].values[0])
+    tomerge.append(val)
+df =  pd.DataFrame(tomerge,columns=['locx','locy'])
+
+
+traject = traject.reset_index(drop=True)
+result = traject.join(df)
+
+
 corrframe['time']=corrframe['time'].astype('int')
-merged = mergecorrectiontraject(Fullframe,corrframe,tijden)
-merged.drop('private', inplace=True, axis=1)
+merged = mergecorrectiontraject(result,corrframe,tijden)
+
+
+gps = pd.DataFrame(merged[' latitude'])
+gps2 = pd.DataFrame(merged[' longitude'])
+gps3 = pd.DataFrame(columns=["latitude","longitude"])
+gps3 = gps2.join(gps)
+
+
+merged.drop(" private", inplace=True, axis=1)
+
+# hier nog gps naar x en y invoegen  
+
 
 target_columns = ['locx','locy'] 
+
 predictors = list(set(list(merged.columns))-set(target_columns))
-merged[predictors] = merged[predictors]/merged[predictors].max()
+
+merged[predictors] = merged[predictors]/merged[predictors].max() # normalize value 
+
 merged.describe().transpose()
 
+
+
 X = merged[predictors].values
+
 y = merged[target_columns].values
+
+#lengte = int(X.shape[0] * 0.6)
+#X_train,X_test = np.array_split(X,2)
+#y_train,y_test = np.array_split(y,2)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=40)
 
 
 
-# evaluate model
-#results = evaluate_model(X, y) kan gebruikt worden voor resultaten functie
-# summarize performance
-#print('MAE: %.3f (%.3f)' % (mean(results), std(results))) # kan gebruikt worden om data te verkrijgen van de analyse
-#mlp.fit(X_train,y_train)
-
-
-# mulkti output model invoegen 
-
-
-
-# Model with n_neurons = inputshape Timestamps, each with x_train.shape[2] variables
-
-# https://machinelearningmastery.com/multi-output-regression-models-with-python/
-
-# define model
+# define base model
 model = LinearSVR()
-# define the direct multioutput wrapper model
-wrapper = MultiOutputRegressor(model)
+# define the chained multioutput wrapper model
+wrapper = RegressorChain(model)
 # fit the model on the whole dataset
 wrapper.fit(X_train, y_train)
-# make a single prediction
 
-yhat = wrapper.predict(X_test)
-# summarize the prediction
-print('Predicted: %s' % yhat)
+results = wrapper.predict(X_test)
+mea = mean_absolute_error(y_test,results)
+mse = mean_squared_error(y_test,results)
+rmse = np.sqrt(mse)
+mape = mean_absolute_percentage_error(y_test,results)
+print("Mean absolute error:",mea,"\n","Mean squared error:",mse,"\n","Root mean squared error:",rmse,"\n","Mean absolute percentage error:",mape*100)
+
+
+
+gps3.rename(columns = {' longitude':'longitude', ' latitude':'latitude'}, inplace = True)
+xgps = []
+ygps=[]
+
+for index in gps3.index:
+    xgps.append(gps3.longitude[index])
+    ygps.append(gps3.latitude[index])
+
+gps3.to_csv("gps.csv")
+
+xs =[]
+ys =[]
+xs1 =[]
+ys1 =[]
+
+
+for x in y_test:
+    xs.append(x[0])
+    ys.append(x[1])
+
+for x in results:
+    xs1.append(x[0])
+    ys1.append(x[1])
+
+plt.figure()
+fig,ax = plt.subplots()
+plt.plot(xs,ys,label= 'UWB')
+plt.plot(xs1,ys1, color = 'red',label='predicted')
+plt.xlim(0,12)
+plt.ylim(0,17)
+plt.xlabel("Metres")
+plt.ylabel("Metres")
+img = plt.imread("veld3.png")
+plt.legend()
+ax.imshow(img, extent=[0, 12, 0, 17])
+
+
+
+
+plt.figure()
+
+plt.plot(xgps,ygps, color = 'red',label='Raw GPS')
+plt.xlabel("longitude")
+plt.ylim = (52.01998,52.02016)
+plt.xlim = (4.26848,4.26876)
+plt.ylabel("latitude")
+plt.show()
+# https://machinelearningmastery.com/multi-output-regression-models-with-python/
+
+
